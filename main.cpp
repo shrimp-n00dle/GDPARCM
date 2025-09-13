6527dfbc669613790e92e0392139dca2e14f000d
shrimp-n00dle <vingno.elizabeth@gmail.com>
2025-09-13 20:16:30 +0800

    BASE

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
-#include "BaseRunner.h"
-int main() {
-	BaseRunner runner;
-	runner.run();
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    /*Default resolution is 1920 x 1080
+    Change before submitting
+    Maximum Screen is 1366 x 768*/
+
+    sf::RenderWindow window(sf::VideoMode(1000, 500), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }

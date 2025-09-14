9bf257c3e19daa433573d883c8d3744b3df65774
shrimp-n00dle <vingno.elizabeth@gmail.com>
2025-09-14 17:47:26 +0800

    [PHASE 0] [55%] Added FPS (little wonky)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "Factory.hpp"
 #include "Base.hpp"
 
+const float FRAME_RATE = 60;
+const sf::Time TIME_PER_FRAME = sf::seconds(1.f / FRAME_RATE);
+
 int main()
 {
     /*Default resolution is 1920 x 1080
@@ -25,21 +28,36 @@ int main()
     const sf::Time TimePerFrame = sf::seconds(1.f / 60.0f);
     Factory::getInstance()->initializeAssets();
     sf::Clock clock;
+    sf::Clock frameClock;
     /*to avoid lagging and help with collision detection*/
     sf::Time timeSinceLastUpdate = sf::Time::Zero;
     Base base;
 
+    window.setFramerateLimit(60);
+
     /*Main Game Loop*/
     while (window.isOpen())
     {
+
         base.processEvents(&window);
+
+        //Add cmd prompt print for now
+        sf::Time frameRate = frameClock.getElapsedTime();
+        std::cout << 1.0f / frameRate.asSeconds() << std::endl;
+        frameClock.restart().asSeconds();
+
+
         timeSinceLastUpdate += clock.restart();
         while (timeSinceLastUpdate > TimePerFrame)
         {
             timeSinceLastUpdate -= TimePerFrame;
             base.processEvents(&window);
             base.performUpdates(TimePerFrame);
+
+           
         }
+
+
 
         /*Draw and Render*/
         Renderer::getInstance()->drawAssets(&window);    

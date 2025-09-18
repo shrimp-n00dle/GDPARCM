81c2b985525a22b254dc80ea8e4622f66c22d2bb
shrimp-n00dle <vingno.elizabeth@gmail.com>
2025-09-18 17:23:58 +0800

    eh

--- main.cpp
+++ main.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
 const int threadCount = 10; //No of threads to create
 const int runTime = 1000; //ms
 
-int threadNum = 0;
-
-int printThread()
+int getThreadNum()
 {
     return rand()% threadCount + 0;
 }
 
-
 void testThread()
 {
-    //and will continously print different things for runTime time
-    for (int i = 0; i < threadCount; i++)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(runTime));
-        threadNum = printThread();
-        std::cout << "I am thread: " <<  threadNum << std::endl;
-    }
- 
+    std::cout << "I am thread: " << getThreadNum() << std::endl;
 }
 
+/*References used:
+https://madhawapolkotuwa.medium.com/understanding-c-threads-a-complete-guide-7e783b22da6b
+https://stackoverflow.com/questions/24220778/simultaneous-threads-in-c-using-thread
+*/
 int main()
 {
     //working behind the scenes
     std::thread myThread(testThread);
 
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < threadCount; i++)
+    {
+        threads.push_back(std::thread(testThread));
+    }
+
+    for (auto& thread : threads)
+    {
+        thread.join();
+    }
+
+
+
     //wait for the threads to finish before exceuting the lines below
     myThread.join();
+
 
     std::cout << "End Main" << std::endl;
 
